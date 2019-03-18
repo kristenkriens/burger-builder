560559ec7349ac89bf46a5bf094ddcabf8e48296
Kristen Kriens <kristen@candyboxmarketing.com>
2019-03-18 14:06:37 -0400

    Updates

---
+++ precache-manifest.8e212c6ccdbbc9c5d66d0d2cd8f6e7b3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "40f616b63a35ec18730d",
+    "url": "/burger-builder/static/css/4.0399f4a4.chunk.css"
+  },
+  {
+    "revision": "4326d8179c5fa466ddd4",
+    "url": "/burger-builder/static/css/main.0cb557f5.chunk.css"
+  },
+  {
+    "revision": "50e73ca0e6c41295622f",
+    "url": "/burger-builder/static/js/runtime~main.50e73ca0.js"
+  },
+  {
+    "revision": "7072e554941af192b6ed",
+    "url": "/burger-builder/static/js/2.7072e554.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "2c2057744afb3fb18fc0",
+    "url": "/burger-builder/static/js/3.2c205774.chunk.js"
+  },
+  {
+    "revision": "4326d8179c5fa466ddd4",
+    "url": "/burger-builder/static/js/main.4326d817.chunk.js"
+  },
+  {
+    "revision": "40f616b63a35ec18730d",
+    "url": "/burger-builder/static/js/4.40f616b6.chunk.js"
+  },
+  {
+    "revision": "3fc1a9e3e0f571a5a6a3",
+    "url": "/burger-builder/static/js/5.3fc1a9e3.chunk.js"
+  },
+  {
+    "revision": "3fc1a9e3e0f571a5a6a3",
+    "url": "/burger-builder/static/css/5.c9ff16e6.chunk.css"
+  },
+  {
+    "revision": "2c2057744afb3fb18fc0",
+    "url": "/burger-builder/static/css/3.269e66e4.chunk.css"
+  },
+  {
+    "revision": "f0afbff008b9043f716ae91ae0301841",
+    "url": "/burger-builder/index.html"
+  }
+];
--- src/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const myAxios = axios.create({
-  baseURL: 'https://burger-builder-6574c.firebaseio.com/'
-});
-
-export default myAxios;
